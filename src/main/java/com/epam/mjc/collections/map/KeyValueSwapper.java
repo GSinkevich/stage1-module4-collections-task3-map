52bf450f67dc360a6fe91210d185d45bc13a2dbf
Yury <sinkevich.work@gmail.com>
2023-09-15 22:32:57 +0300

    task #2 done

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,9 +1,29 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
-        return  null;
+        Map<String, Integer> result = new HashMap<>();
+
+
+        for (var ObjectSourceMap : sourceMap.entrySet()
+        ) {
+            String valueSourceMap = ObjectSourceMap.getValue();
+            if (result.get(valueSourceMap) == null) {
+                result.put(valueSourceMap, ObjectSourceMap.getKey());
+            } else {
+
+                Integer currentValueInResult = result.get(valueSourceMap);
+
+                if (currentValueInResult > ObjectSourceMap.getKey()) {
+                    result.put(valueSourceMap, ObjectSourceMap.getKey());
+                }
+            }
+        }
+        return result;
     }
+
 }
