054ee5b6e9a77b90e126baa0775b6a6cad5f2059
Yury <sinkevich.work@gmail.com>
2023-09-16 16:39:45 +0300

    task #3 done

--- src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
+++ src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
@@ -1,15 +1,23 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FunctionValueFinder {
     public boolean isFunctionValuePresent(List<Integer> sourceList, int requiredValue) {
         Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
-        return  true;
+        return functionMap.containsValue(requiredValue);
     }
 
     private Map<Integer, Integer> calculateFunctionMap(List<Integer> sourceList) {
-        return  null;
+        Map<Integer, Integer> functionMap = new HashMap<>();
+        for (Integer currentDigital : sourceList
+        ) {
+            Integer resultFunction = 5 * currentDigital + 2;
+            functionMap.put(currentDigital, resultFunction);
+        }
+
+        return functionMap;
     }
 }
